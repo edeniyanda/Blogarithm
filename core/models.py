315678b6a6f739503b31f70dc9c360417aeca6e2
Eden Iyanda <edeniyanda@gmail.com>
2024-07-29 21:30:49 +0100

    Update: Added cover image field to Post Model

--- core/models.py
+++ core/models.py
@@ -5,8 +5,11 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    cover_img = models.ImageField(upload_to="post_images", blank=True, null=True)
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    
+
+    def __str__(self) -> str:
+        return f"{self.title} by {self.author}"
 
 
